f71951e73c502bf561c206f621da9cc4655b7f9c
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2022-09-21 01:51:23 +0700

    simultan animasi

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,24 @@ class AnimatedLogo extends AnimatedWidget {
   const AnimatedLogo({super.key, required Animation<double> animation})
       : super(listenable: animation);
 
+  // implementasi simultan animasi
+  // Make the Tweens static because they don't change.
+  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
+  static final _sizeTween = Tween<double>(begin: 0, end: 300);
+
   @override
   Widget build(BuildContext context) {
     final animation = listenable as Animation<double>;
 
     return Center(
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 10),
-        height: animation.value,
-        width: animation.value,
-        child: const FlutterLogo(),
+      child: Opacity(
+        opacity: _opacityTween.evaluate(animation),
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          height: _sizeTween.evaluate(animation),
+          width: _sizeTween.evaluate(animation),
+          child: const FlutterLogo(),
+        ),
       ),
     );
   }
@@ -49,20 +57,30 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     //   });
 
     // implementasi animated widget
-    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    // animation = Tween<double>(begin: 0, end: 300).animate(controller);
+
+    // implementasi simultan animasi
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
     controller.forward();
   }
 
   @override
   Widget build(BuildContext context) {
     // implementasi animated builder
-    return GrowTransition(
-      animation: animation,
-      child: const LogoWidget(),
-    );
+    // return GrowTransition(
+    //   animation: animation,
+    //   child: const LogoWidget(),
+    // );
 
     // implementasi animated widget
-    // return AnimatedLogo(animation: animation);
+    return AnimatedLogo(animation: animation);
 
     // return Center(
     //   child: Container(
